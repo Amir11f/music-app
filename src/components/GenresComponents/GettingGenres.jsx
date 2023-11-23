d768fef6be94bed2fd7258078cc0eaef163159f6
amirhossain <zohrabiamirhossain@gmail.com>
2023-11-23 12:46:29 +0330

    genres update

--- src/components/GenresComponents/GettingGenres.jsx
+++ src/components/GenresComponents/GettingGenres.jsx
@@ -2,25 +2,30 @@
 import axios from "axios";
 import { useState , useEffect} from "react";
 
-export async function GettingGenres(){
+export function GettingGenres(){
     const[genres , setGenres]= useState()
 
-    const url = 'https://spotify23.p.rapidapi.com/genre_view/?id=0JQ5DAqbMKFEC4WFtoNRpw&content_limit=10&limit=20';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'f92bb4cf7bmsh39095ddd4360300p18573djsnd00a9ba35bf4',
-            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-        }
-    };
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+    // const url = 'https://spotify23.p.rapidapi.com/genre_view/?id=0JQ5DAqbMKFEC4WFtoNRpw&content_limit=10&limit=20';
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'f92bb4cf7bmsh39095ddd4360300p18573djsnd00a9ba35bf4',
+    //         'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    //     }
+    // };
+    //     try {
+    //         const response = await fetch(url, options);
+    //         const result = await response.text();
+    //         console.log(result);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
 
+        async function clickedButton(){
+            const get = await axios.get(`https://connect.deezer.com/oauth/auth.php?app_id=643701&redirect_uri=http://localhost3000&perms=basic_access,email`)
+            consule.log('fuckckkck')
+            return get
+        }
 
 
     
@@ -28,6 +33,10 @@ export async function GettingGenres(){
     return(
         <>
         <h1>hello</h1>
+        <button onClick={clickedButton} classname='bg-red color-blue'>
+            ckick here fucker
+        </button>
+        {clickedButton}
         </>
     )
 
